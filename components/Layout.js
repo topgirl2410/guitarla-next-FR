cb5ce8fbdd9995671fab506fba9be98d7ce6d0e2
topgirl2410 <maria.topgirl82@gmail.com>
2022-09-06 21:18:54 +0200

    primeros pasos con el header

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,19 @@
 import Head from "next/head"
+import Header from "../components/Header"
+
+
+
 
 const Layout = ({ children, pagina }) => {
     return (
         <div>
             <Head>
                 <title>GuitarLA - {pagina}</title>
-
+                <meta name="description" content="Sito Web de Ventas de Guitarras" />
             </Head>
 
 
+            <Header />
             {children}
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
       pagina='Home'
     >
       <h1>Desde Inicio...</h1>
+
     </Layout>
 
   )

a26d9ba1a5c979404f98b56b736f2a20e4da54b6
topgirl2410 <maria.topgirl82@gmail.com>
2022-09-06 22:08:39 +0200

    el componente Image de Next.js

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,28 @@
 import Link from "next/link"
-
+import Image from "next/image"
 import styles from "../styles/Header.module.css"
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <div className="contenedor">
-                <div>
+                <div className={styles.barra}>
+                    <Link href="/">
+                        <Image
+                            src="/img/logo.svg"
+                            width={400}
+                            height={100}
+                            alt='Imagen logo'
+                        />
+                    </Link>
 
+                    <nav className={styles.navegacion}>
+                        <Link href='/'>Inicio</Link>
+                        <Link href='/nosotros'>Nosotros</Link>
+                        <Link href='/blog'>Blog</Link>
+                        <Link href='/tienda'>Tienda</Link>
+                    </nav>
                 </div>
-
-                <nav>
-                    <Link href='/'>Inicio</Link>
-                    <Link href='/nosotros'>Nosotros</Link>
-                    <Link href='/blog'>Blog</Link>
-                    <Link href='/tienda'>Tienda</Link>
-                </nav>
             </div>
 
         </header>
